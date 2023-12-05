6efd384f55d30fcdb26bc72a2252689c0f44e562
Oliver Lassen <TryingToImprove@users.noreply.github.com>
2023-12-05 08:55:35 +0100

    Add failing test case

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,26 @@
-/** Add your relevant code here for the issue to reproduce */
+import { TEXT } from "./CONTENT";
+
 export default function Home() {
-  return null
+  async function onSubmit(formData: FormData) {
+    "use server";
+
+    const item = formData.get("text");
+
+    if (typeof item === "string" && item.length !== TEXT.length) {
+      throw new Error(
+        `Expected item.length to be ${TEXT.length}, was ${
+          item.length
+        }; Around ${
+          Buffer.from(item).byteLength / 1024 / 1024
+        }MB, but text is ${Buffer.from(TEXT).length / 1024 / 1024}MB`
+      );
+    }
+  }
+
+  return (
+    <form action={onSubmit}>
+      <input type="hidden" name="text" value={TEXT} />
+      <button type="submit">SUBMIT</button>
+    </form>
+  );
 }
